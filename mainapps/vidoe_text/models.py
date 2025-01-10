ea35ba1d70e064058c80d956e02a4d97e2caac4b
UbongPr7 <ubongpr7@gmail.com>
2025-01-10 07:22:14 +0000

    timezone fix

--- mainapps/vidoe_text/models.py
+++ mainapps/vidoe_text/models.py
@@ -14,6 +14,7 @@ import re
 from threading import Timer
 from django.core.files import File
 from django.conf import settings
+from django.utils import timezone
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
